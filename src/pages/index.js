871fad3dca19d4ea2c9cea7d7c39bd51746e755f
kurt miller <kmiller9393@gmail.com>
2019-09-07 12:50:23 -0600

    Update index.js page within pages directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { withRouteData } from 'react-static';
+import RecentPosts from '../frontend/components/RecentPosts/RecentPosts';
+import Footer from '../frontend/components/Footer/Footer';
+import Navbar from '../frontend/components/Navbar/Navbar';
+import signature from '../frontend/images/signature.svg';
 
-export default () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
-  </div>
-)
+class index extends Component {
+  getPosts = () => {
+    const { posts } = this.props;
+    let newPosts = [];
+
+    for (let i = 1; i <= 3; i++) {
+      if (posts[posts.length - i]) {
+        newPosts.push(posts[posts.length - i]);
+      }
+    }
+
+    return newPosts;
+  };
+
+  render() {
+    const { handle } = this.props.authors[0].avatar;
+    const { bibliography } = this.props.authors[0];
+
+    const recentPosts = this.getPosts();
+
+    return (
+      <div className="home-container">
+        <Navbar />
+        <article className="image-container banner-image">
+          <aside className="author-image-container">
+            <img
+              className="author-image"
+              src={`https://media.graphcms.com/${handle}`}
+              alt="Kimaleen"
+            />
+          </aside>
+          <aside className="bio-text">
+            <p>{bibliography}</p>
+            <img src={signature} className="signature" alt="signature" />
+          </aside>
+        </article>
+        <RecentPosts posts={recentPosts} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withRouteData(index);
