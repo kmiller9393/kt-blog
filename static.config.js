4ba796f69f80f9801eda3f094fdba389cdb117db
kurt miller <kmiller9393@gmail.com>
2019-09-07 12:11:40 -0600

    Update static.config.js file so pages are filtered properly based on content

--- static.config.js
+++ static.config.js
@@ -1,36 +1,148 @@
-import path from 'path'
-import axios from 'axios'
+import { request } from 'graphql-request';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const GRAPHCMS_ENDPOINT =
+  'https://api-uswest.graphcms.com/v1/cjvjziu5s627901ehblbcdtu9/master';
+
+const query = `
+{
+  posts{
+    id
+    title
+    image{
+      handle
+    }
+    content
+    tag
+    author{
+      id
+      name
+    }
+  }
+  
+  authors{
+    id
+    name
+    avatar{
+      handle
+    }
+    bibliography
+  }
+}
+`;
 
 export default {
+  getSiteData: () => ({
+    title: 'Kimaleen Tran'
+  }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
+    const { authors, posts } = await request(GRAPHCMS_ENDPOINT, query);
 
     return [
       {
-        path: '/blog',
+        path: '/',
+        component: 'src/pages/index.js',
         getData: () => ({
           posts,
+          authors
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
-          template: 'src/containers/Post',
+          template: 'src/pages/post',
           getData: () => ({
-            post,
-          }),
-        })),
+            post
+          })
+        }))
       },
-    ]
+      {
+        path: '/blog',
+        getData: () => ({
+          posts
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/pages/post',
+          getData: () => ({
+            post
+          })
+        }))
+      },
+      {
+        path: '/thoughts',
+        component: 'src/pages/thoughts',
+        getData: () => {
+          let thoughtsPosts = posts.filter(post => post.tag === 'Thoughts');
+
+          return { posts: [...thoughtsPosts] };
+        },
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/pages/post',
+          getData: () => ({
+            post
+          })
+        }))
+      },
+      {
+        path: '/lifestyle',
+        component: 'src/pages/lifestyle',
+        getData: () => {
+          let lifestylePosts = posts.filter(post => post.tag === 'Lifestyle');
+
+          return { posts: [...lifestylePosts] };
+        },
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/pages/post',
+          getData: () => ({
+            post
+          })
+        }))
+      },
+      {
+        path: '/travel',
+        component: 'src/pages/travel',
+        getData: () => {
+          let travelPosts = posts.filter(post => post.tag === 'Travel');
+
+          return { posts: [...travelPosts] };
+        },
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/pages/post',
+          getData: () => ({
+            post
+          })
+        }))
+      },
+      {
+        path: '/food',
+        component: 'src/pages/food',
+        getData: () => {
+          let foodPosts = posts.filter(post => post.tag === 'Food');
+
+          return { posts: [...foodPosts] };
+        },
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/pages/post',
+          getData: () => ({
+            post
+          })
+        }))
+      }
+    ];
   },
   plugins: [
     [
       require.resolve('react-static-plugin-source-filesystem'),
       {
-        location: path.resolve('./src/pages'),
-      },
+        location: path.resolve('./src/pages')
+      }
     ],
     require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
-  ],
-}
+    require.resolve('react-static-plugin-sitemap')
+  ]
+};
